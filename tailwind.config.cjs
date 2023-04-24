163462c9fcb8a2462add3a38b64edefa835911d5
Julien Déramond <juderamond@gmail.com>
2023-04-24 22:16:55 +0200

    feat(chapters): add previous/next buttons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		screens: {
+			'xs': '480px',
+			...defaultTheme.screens
+		}
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
