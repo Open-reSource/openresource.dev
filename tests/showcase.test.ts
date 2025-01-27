ad0116b364ac535b470d24271a44ea830c8ea210
Julien Déramond <juderamond@gmail.com>
2025-01-27 15:38:48 +0100

    fix(tests): `AssertionError: expected false to be true // Object.is equality` (#1083)
    Related test: 'should delete the existing showcase content collection before saving showcases to handle deleted comments'

--- tests/showcase.test.ts
+++ tests/showcase.test.ts
@@ -192,7 +192,7 @@ test('should delete the existing showcase content collection before saving showc
 	expect(mkdirMock).toHaveBeenCalledOnce();
 	expect(writeFileMock).toHaveBeenCalledOnce();
 	expect(rmMock.mock.invocationCallOrder < mkdirMock.mock.invocationCallOrder).toBe(true);
-	expect(mkdirMock.mock.invocationCallOrder < writeFileMock.mock.invocationCallOrder).toBe(true);
+	expect(mkdirMock).toHaveBeenCalledBefore(writeFileMock);
 });
 
 test('should save a showcase file per user', async () => {
