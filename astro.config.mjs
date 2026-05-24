64b4c449fdd48d4aeffe576399184a15c24ab8f8
Julien Déramond <juderamond@gmail.com>
2026-05-24 11:18:36 +0200

    Fix Starlight 0.39 issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -364,7 +364,7 @@ export default defineConfig({
 				},
 				{
 					label: 'Resources',
-					autogenerate: { directory: 'resources' },
+					items: [{ autogenerate: { directory: 'resources' } }],
 				},
 				{
 					label: 'Articles',
